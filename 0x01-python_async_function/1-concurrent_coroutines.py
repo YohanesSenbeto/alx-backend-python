dbd303ccfbdbf01c025474c9318414432c0a04af
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-03-19 12:45:09 +0000

    Updated asysnc function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,23 +4,14 @@ Module for executing multiple coroutines concurrently
 """
 
 import asyncio
-import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """
-    Asynchronous coroutine that waits for a random delay between 0 and max_del
-    seconds and eventually returns it.
-    """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronous routine that spawns wait_random n times with the specified m
     Returns the list of all the delays (float values) in ascending order.
     """
-    delays = [await wait_random(max_delay) for _ in range(n)]
-    return sorted(delays)
+    return sorted([await wait_random(max_delay) for _ in range(n)])
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import time
+import asyncio
 from typing import List
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
+def measure_time(max_delay: int = 10, n: int = 0) -> float:
     """
     Measures the total execution time for wait_n(n, max_delay) and returns
 
@@ -18,11 +19,9 @@ def measure_time(n: int, max_delay: int) -> float:
         The average execution time per call in seconds (float).
     """
 
-    start_time = time.time()
+    first_time = time.perf_counter()
+    asyncio.run(wait_n(max_delay, n))
+    elapsed_time = time.perf_counter() - first_time
+    total_time = elapsed_time / n
 
-    wait_n(n, max_delay)
-
-    end_time = time.time()
-    total_time = end_time - start_time
-    average_time = total_time / n
-    return average_time
+    return total_time
